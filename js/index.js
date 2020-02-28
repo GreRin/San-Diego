4cf7d474421f267d92483ac4739014483079d7da
Ryhor Sidoryn <rigor1988@gmail.com>
2020-02-28 16:10:29 +0300

    feat: finish aside section and start form

--- js/index.js
+++ js/index.js
@@ -1,19 +1,23 @@
 const signIn = document.getElementById('sign-in');
 const back = document.getElementById('back');
-const sections = document.getElementsByTagName('section');
+const main = document.getElementsByTagName('main');
+const aside = document.getElementsByTagName('aside');
+const header = document.getElementsByTagName('header');
+const footer = document.getElementsByTagName('footer');
 
 signIn.addEventListener('click', (event) => {
-	console.log(sections);
-	for (let i=0; i<sections.length; ++i) {
-		sections[i].classList.add('hidden')
-		signIn.removeEventListener('click', (event), true);
-	}
+	console.log(aside);
+	main[0].classList.add('hidden');
+	aside[0].classList.remove('hidden');
+	header[0].classList.add('header-form');
+	footer[0].classList.add('footer-form');
+	signIn.removeEventListener('click', (event), true);
 }, true);
 
 back.addEventListener('click', (event) => {
-	// const sections = document.getElementsByTagName('section');
-	for (let i=0; i<sections.length; ++i) {
-		sections[i].classList.remove('hidden')
-		back.removeEventListener('click', (event), true);
-	}
+	main[0].classList.remove('hidden');
+	aside[0].classList.add('hidden');
+	header[0].classList.remove('header-form');
+	footer[0].classList.remove('footer-form');
+	signIn.removeEventListener('click', (event), true);
 }, true);
