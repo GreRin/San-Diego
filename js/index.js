79771df5b9d6713a6ec274ad126f63036e142b9e
Ryhor Sidoryn <rigor1988@gmail.com>
2020-02-28 21:42:18 +0300

    feat: make facebook and google buttons

--- js/index.js
+++ js/index.js
@@ -4,13 +4,14 @@ const main = document.getElementsByTagName('main');
 const aside = document.getElementsByTagName('aside');
 const header = document.getElementsByTagName('header');
 const footer = document.getElementsByTagName('footer');
+const form = document.getElementById('form');
 
 signIn.addEventListener('click', (event) => {
-	console.log(aside);
 	main[0].classList.add('hidden');
 	aside[0].classList.remove('hidden');
 	header[0].classList.add('header-form');
 	footer[0].classList.add('footer-form');
+	form.classList.remove('hidden');
 	signIn.removeEventListener('click', (event), true);
 }, true);
 
@@ -19,5 +20,6 @@ back.addEventListener('click', (event) => {
 	aside[0].classList.add('hidden');
 	header[0].classList.remove('header-form');
 	footer[0].classList.remove('footer-form');
+	form.classList.add('hidden');
 	signIn.removeEventListener('click', (event), true);
 }, true);
