779fa54e8be17355389519f85c4936b9eed13954
Ryhor Sidoryn <rigor1988@gmail.com>
2020-03-01 15:53:23 +0300

    feat: add toushstart eventListener for mobiles and tablets

--- js/index.js
+++ js/index.js
@@ -18,6 +18,18 @@ signIn.addEventListener('click', (event) => {
 	signIn.removeEventListener('click', (event), true);
 }, true);
 
+signIn.addEventListener('touchstart', (event) => {
+	main[0].classList.add('hidden');
+	aside[0].classList.remove('hidden');
+	header[0].classList.add('header-form');
+	header[0].style.position = 'static';
+	footer[0].classList.add('footer-form');
+	footer[0].style.position = 'static';
+	form.classList.remove('hidden');
+	form.style.margin = '0';
+	signIn.removeEventListener('touchstart', (event), true);
+}, true);
+
 back.addEventListener('click', (event) => {
 	main[0].classList.remove('hidden');
 	aside[0].classList.add('hidden');
@@ -27,6 +39,17 @@ back.addEventListener('click', (event) => {
 	footer[0].style.position = 'fixed';
 	form.classList.add('hidden');
 	signIn.removeEventListener('click', (event), true);
+}, true);
+
+back.addEventListener('touchstart', (event) => {
+	main[0].classList.remove('hidden');
+	aside[0].classList.add('hidden');
+	header[0].classList.remove('header-form');
+	header[0].style.position = 'fixed';
+	footer[0].classList.remove('footer-form');
+	footer[0].style.position = 'fixed';
+	form.classList.add('hidden');
+	signIn.removeEventListener('touchstart', (event), true);
 }, true);
 
 password = document.getElementById('password');
