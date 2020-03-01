cccefca8161637ec1118ede96b9d03ad00f21078
Ryhor Sidoryn <rigor1988@gmail.com>
2020-03-01 15:33:10 +0300

    feat: make all adaptive

--- js/index.js
+++ js/index.js
@@ -10,8 +10,11 @@ signIn.addEventListener('click', (event) => {
 	main[0].classList.add('hidden');
 	aside[0].classList.remove('hidden');
 	header[0].classList.add('header-form');
+	header[0].style.position = 'static';
 	footer[0].classList.add('footer-form');
+	footer[0].style.position = 'static';
 	form.classList.remove('hidden');
+	form.style.margin = '0';
 	signIn.removeEventListener('click', (event), true);
 }, true);
 
@@ -19,7 +22,9 @@ back.addEventListener('click', (event) => {
 	main[0].classList.remove('hidden');
 	aside[0].classList.add('hidden');
 	header[0].classList.remove('header-form');
+	header[0].style.position = 'fixed';
 	footer[0].classList.remove('footer-form');
+	footer[0].style.position = 'fixed';
 	form.classList.add('hidden');
 	signIn.removeEventListener('click', (event), true);
 }, true);
@@ -27,6 +32,6 @@ back.addEventListener('click', (event) => {
 password = document.getElementById('password');
 show = document.getElementById('show');
 show.addEventListener('click', (event) => {
-		(password.getAttribute('type') === 'text') ? password.setAttribute('type', 'password') : password.setAttribute('type', 'text');
-		password.removeEventListener('click', (event), true);
-	}, true);
+	(password.getAttribute('type') === 'text') ? password.setAttribute('type', 'password') : password.setAttribute('type', 'text');
+	password.removeEventListener('click', (event), true);
+}, true);
